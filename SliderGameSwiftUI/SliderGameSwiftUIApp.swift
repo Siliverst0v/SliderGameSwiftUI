955048659fd9fe1f00f8a81bf515a31292b76cc0
Anatoly Siliverstov <siliverst0v@yandex.ru>
2022-04-07 23:28:11 +0300

    Initial Commit

---
+++ SliderGameSwiftUI/SliderGameSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderGameSwiftUIApp.swift
+//  SliderGameSwiftUI
+//
+//  Created by Анатолий Силиверстов on 07.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct SliderGameSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
