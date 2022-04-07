58faedf356275a8481577cf88671d7290600074c
Anatoly Siliverstov <siliverst0v@yandex.ru>
2022-04-08 00:06:42 +0300

    Final

--- SliderGameSwiftUI/ContentView.swift
+++ SliderGameSwiftUI/ContentView.swift
@@ -8,9 +8,55 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var targetValue = Int.random(in: 0...100)
+    @State private var currnetValue = 50.0
+    @State private var showAlert = false
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            GameSlider(
+                currentValue: $currnetValue,
+                targetValue: targetValue,
+                color: .red,
+                alpha: computeScore()
+            )
+            ButtonView(
+                showAlert: $showAlert,
+                title: "Проверь меня",
+                currentScore: computeScore(),
+                action: { showAlert = true }
+            )
+            ButtonView(title: "Начать заново") {
+                targetValue = Int.random(in: 0...100)
+            }
+        }
+    }
+    
+    private func computeScore() -> Int {
+        let difference = abs(targetValue - lround(currnetValue))
+        return 100 - difference
+    }
+}
+
+struct ButtonView: View {
+    @Binding var showAlert: Bool
+    let title: String
+    let currentScore: Int
+    let action: () -> Void
+    
+    var body: some View {
+        Button(title, action: action)
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Your Score"), message: Text("\(currentScore)"))
+            }
             .padding()
+    }
+    
+    init(showAlert: Binding<Bool> = .constant(false), title: String, currentScore: Int = 0, action: @escaping () -> Void) {
+        self._showAlert = showAlert
+        self.title = title
+        self.currentScore = currentScore
+        self.action = action
     }
 }
 
---
+++ SliderGameSwiftUI/CustomSlider.swift
@@ -0,0 +1,58 @@
+//
+//  CustomSlider.swift
+//  SliderGameSwiftUI
+//
+//  Created by Анатолий Силиверстов on 07.04.2022.
+//
+
+import SwiftUI
+
+struct CustomSlider: UIViewRepresentable {
+    @Binding var value: Double
+    
+    let alpha: Int
+    let color: UIColor
+    
+    func makeUIView(context: Context) -> UISlider {
+        let slider = UISlider(frame: .zero)
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        
+        slider.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.valueChanged),
+            for: .valueChanged
+        )
+        return slider
+    }
+    
+    func updateUIView(_ uiView: UISlider, context: Context) {
+        uiView.thumbTintColor = color.withAlphaComponent(CGFloat(alpha)/100)
+        uiView.value = Float(value)
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(value: $value)
+    }
+    
+}
+
+extension CustomSlider {
+    class Coordinator: NSObject {
+        @Binding var value: Double
+        
+        init(value: Binding<Double>) {
+            self._value = value
+        }
+        
+        @objc func valueChanged(_ sender: UISlider) {
+            value = Double(sender.value)
+        }
+    }
+}
+
+struct CustomSlider_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomSlider(value: .constant(50), alpha: 100, color: .blue)
+    }
+}
